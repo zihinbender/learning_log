d411662449f0768a95f6151976fa93437f70b7c8
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-11-13 13:52:01 +0000

    Setting up user accounts

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps.
     "learning_logs",
+    "accounts",
     # Default django apps.
     "django.contrib.admin",
     "django.contrib.auth",
@@ -123,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
