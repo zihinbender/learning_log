eb5387fe6cad2a0d1753cb3d34e67b2afd8f6af0
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-12-10 13:38:01 +0000

    Project completed with bootsrap5 and other implementations

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     "learning_logs",
     "accounts",
 
+    # Third party apps.
+    "django_bootstrap5",
+
     # Default Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
